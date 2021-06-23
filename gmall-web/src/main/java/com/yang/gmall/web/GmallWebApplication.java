2c378e4b75af98be3e9b713fef3d19b161691ac9
yangchao <yangchao@cqu.com>
2021-06-23 15:06:08 +0800

    项目结构创建完成！

---
+++ gmall-web/src/main/java/com/yang/gmall/web/GmallWebApplication.java
@@ -0,0 +1,13 @@
+package com.yang.gmall.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallWebApplication.class, args);
+    }
+
+}
