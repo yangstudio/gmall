c491dea4292b9b21192bbbe93fc4d6de5676cb50
yangchao <yangchao@cqu.com>
2021-06-25 19:34:47 +0800

    项目结构创建完成！

--- gmall-pms/src/main/java/com/yang/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/yang/gmall/pms/GmallPmsApplication.java
@@ -1,8 +1,22 @@
 package com.yang.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1、配置整合dubbo
+ * 2、配置整合MyBatisPlus
+ *
+ * logstash整合，
+ * 1）、导jar
+ * 2）、导日志配置
+ * 3）、在kibana里面建立好日志的索引，就可以可视化检索
+ *
+ */
+@EnableDubbo
+@MapperScan(basePackages = "com.yang.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
